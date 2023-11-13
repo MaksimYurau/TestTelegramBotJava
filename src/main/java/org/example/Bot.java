243054e818cdc5bc4cf1b860a0b892c85fbe9074
MaksimYurau <2004softwaredeveloper@gmail.com>
2023-11-13 11:49:35 +0300

    Updated project. Updated Bot.java. Updated Main.java. Added dice sending mechanism.

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendDice;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -30,12 +31,23 @@ public class Bot extends TelegramLongPollingBot {
 
     public void sendText(Long who, String what){
         SendMessage sm = SendMessage.builder()
-                .chatId(who.toString()) //Who are we sending a message to
-                .text(what).build();    //Message content
+                .chatId(who.toString()) // Who are we sending a message to
+                .text(what).build();    // Message content
         try {
-            execute(sm);                        //Actually sending the message
+            execute(sm);                        // Actually sending the message
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);      //Any error will be printed here
+            throw new RuntimeException(e);      // Any error will be printed here
+        }
+    }
+
+    public void sendDice(Long who, String what) {
+        SendDice sd = SendDice.builder()
+                .chatId(who.toString())         // Who are we sending a message to
+                .emoji(what).build();           // Emoji content
+        try {
+            execute(sd);                        // Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      // Any error will be printed here
         }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,9 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        Bot bot = new Bot();                  //We moved this line out of the register method, to access it later
+        Bot bot = new Bot();                  // We moved this line out of the register method, to access it later
         botsApi.registerBot(bot);
-        bot.sendText(692500222L, "Hello World!");  //The L just turns the Integer into a Long
+        bot.sendText(692500222L, "Hello World!");  // The L just turns the Integer into a Long
+        bot.sendDice(692500222L, "\uD83C\uDFAF");  // The L just turns the Integer into a Long
     }
 }
